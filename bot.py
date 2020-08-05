714d547e8613d034dee9563fe30aa07625b2be3f
nicafaynberg <berenis0102@gmail.com>
2020-08-05 23:20:22 +0300

    Change log level

--- bot.py
+++ bot.py
@@ -7,7 +7,7 @@ from dialuge_structure import conversation
 
 # Enable logging
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-                    level=logging.INFO)
+                    level=logging.DEBUG)
 
 logger = logging.getLogger(__name__)
 
